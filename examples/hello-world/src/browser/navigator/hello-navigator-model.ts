d033bb4e108736101fb6012ddb27d1387f64f5da
shanehsi <shane.hsi@foxmail.com>
2024-01-19 22:35:39 +0800

    navigator override

--- examples/hello-world/src/browser/navigator/hello-navigator-model.ts
+++ examples/hello-world/src/browser/navigator/hello-navigator-model.ts
@@ -2,9 +2,16 @@ import { inject, injectable } from '@theia/core/shared/inversify'
 import { FileNavigatorModel } from '@theia/navigator/lib/browser'
 import { FileNavigatorTree } from '@theia/navigator/lib/browser/navigator-tree'
 import { HelloFileNavigatorTree } from './hello-navigator-tree'
+import { TreeNode } from '@theia/core/lib/browser'
 
 @injectable()
 export class HelloFileNavigatorModel extends FileNavigatorModel {
     @inject(HelloFileNavigatorTree) protected override readonly tree: FileNavigatorTree
 
+    override async createRoot(): Promise<TreeNode | undefined> {
+        const root = await super.createRoot()
+        // @ts-ignore
+        root.children[0].fileStat.resource._path.base = 'temp22'
+        return root
+    }
 }
--- examples/hello-world/src/browser/navigator/hello-navigator-tree.ts
+++ examples/hello-world/src/browser/navigator/hello-navigator-tree.ts
@@ -10,17 +10,7 @@ export class HelloFileNavigatorTree extends FileNavigatorTree {
         }
         const children = await super.resolveChildren(parent)
         // @ts-ignore
-        children[0].id = '/Users/shane/Desktop/temp:/Users/shane/Desktop/temp/Untitled11.txt'
-        // @ts-ignore
-        children[0].fileStat.resource.codeUri.path = '/Users/shane/Desktop/temp:/Users/shane/Desktop/temp/Untitled33.txt'
-        // @ts-ignore
-        children[0].fileStat.resource._path.name = 'xx'
-        // @ts-ignore
-        children[0].fileStat.resource._path.raw = '/Users/shane/Desktop/temp/Untitled44.txt'
-        // @ts-ignore
         children[0].fileStat.resource._path.base = 'Untitled5.txt'
-        // @ts-ignore
-        children[0].fileStat.name = 'Untitled22.txt'
         return children
     }
 
