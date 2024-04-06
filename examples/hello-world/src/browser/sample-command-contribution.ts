97395ab7792c35465d971556a4e759cdcb063880
shanehsi <shane.hsi@foxmail.com>
2024-04-06 11:58:28 +0800

    unregister commands

--- examples/hello-world/src/browser/sample-command-contribution.ts
+++ examples/hello-world/src/browser/sample-command-contribution.ts
@@ -4,9 +4,16 @@ import { LoginDialog } from './login/login-dialog'
 import { GridCellCommand } from './resource/resource-grid-model'
 import { NavigatorDiffCommands } from '@theia/navigator/lib/browser/navigator-diff'
 import { FileSystemCommands } from '@theia/filesystem/lib/browser/filesystem-frontend-contribution'
-import { CommonCommands } from '@theia/core/lib/browser'
+import { CommonCommands, KeyboardCommands } from '@theia/core/lib/browser'
 import { WorkspaceCommands } from '@theia/workspace/lib/browser'
 import { ThemeService } from '@theia/core/lib/browser/theming'
+import { WindowCommands } from '@theia/core/lib/browser/window-contribution'
+import { FileNavigatorCommands } from '@theia/navigator/lib/browser/file-navigator-commands'
+import { KeymapsCommands } from '@theia/keymaps/lib/browser'
+import { PreferencesCommands } from '@theia/preferences/lib/browser/util/preference-types'
+import { MiniBrowserCommands } from '@theia/mini-browser/lib/browser/mini-browser-open-handler'
+import { LIST_VARIABLES } from '@theia/variable-resolver/lib/browser/variable-resolver-frontend-contribution'
+import { EditorCommands } from '@theia/editor/lib/browser'
 
 @injectable()
 export class SampleCommandContribution implements CommandContribution {
@@ -49,10 +56,61 @@ export class SampleCommandContribution implements CommandContribution {
       isVisible: () => true,
     })
 
-    commandRegistry.unregisterCommand(NavigatorDiffCommands.COMPARE_FIRST.id)
-    commandRegistry.unregisterCommand(FileSystemCommands.UPLOAD.id)
-    commandRegistry.unregisterCommand(CommonCommands.COPY.id)
-    commandRegistry.unregisterCommand(CommonCommands.PASTE.id)
-    commandRegistry.unregisterCommand(WorkspaceCommands.ADD_FOLDER.id)
+
+    /*
+    目前 theia packages 互相依赖，特别是 monaco 是强依赖
+    过滤依赖可以在 FilterContribution 整体过滤
+    或者通过 commands/menu unregister 细粒度过滤
+    前者的优势是干净彻底，但是可能有些是依赖，即通过 extends 复用
+    后者劣势是繁琐，一般是不能通过 FilterContribution 才进行微调
+     */
+    const unregisterCommands = [
+      LIST_VARIABLES.id,
+
+      ...Object.values(EditorCommands).map(command => command.id),
+      ...Object.values(NavigatorDiffCommands).map(command => command.id),
+      ...Object.values(FileSystemCommands).map(command => command.id),
+      ...Object.values(FileNavigatorCommands).map(command => command.id),
+      ...Object.values(WindowCommands).map(command => command.id),
+      ...Object.values(KeyboardCommands).map(command => command.id),
+
+      ...Object.values(WorkspaceCommands).reduce<string[]>((acc, cur) => {
+        if ('id' in cur && typeof cur.id === 'string') {
+          acc.push(cur.id)
+        }
+        return acc
+      }, []),
+
+      ...Object.values(MiniBrowserCommands).reduce<string[]>((acc, cur) => {
+        if (typeof cur !== 'string') {
+          acc.push(cur.id)
+        }
+        return acc
+      }, []),
+
+      ...Object.values(KeymapsCommands)
+        .filter(command => command.id !== KeymapsCommands.OPEN_KEYMAPS.id)
+        .map(command => command.id),
+      ...Object.values(PreferencesCommands).filter(command => [
+        PreferencesCommands.OPEN_WORKSPACE_PREFERENCES.id,
+      ].indexOf(command.id) === -1)
+        .map(command => command.id),
+
+      ...Object.values(CommonCommands).reduce<string[]>((acc, cur) => {
+        if (typeof cur === 'string') return acc
+
+        if ([
+          CommonCommands.SELECT_COLOR_THEME.id,
+          CommonCommands.OPEN_PREFERENCES.id,
+          CommonCommands.ABOUT_COMMAND.id,
+        ].indexOf(cur.id) > -1) return acc
+
+        acc.push(cur.id)
+        return acc
+      }, []),
+
+      ...['First', 'Second', 'Third', 'Fourth', 'Fifth', 'Sixth', 'Seventh', 'Eighth', 'Ninth'].map(ordinal => `workbench.action.focus${ordinal}EditorGroup`),
+    ]
+    unregisterCommands.forEach(id => commandRegistry.unregisterCommand(id))
   }
 }
